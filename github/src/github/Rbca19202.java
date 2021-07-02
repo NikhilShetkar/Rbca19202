557673d84420cb6df8dc943a83f4e7cd5f86baf7
Nikhil RealMadrid <Nikhil RealMadrid@LAPTOP-098A5AAU>
2021-07-02 15:29:57 +0530

    frist commit

---
+++ github/src/github/Rbca19202.java
@@ -0,0 +1,21 @@
+package github;
+
+public class Rbca19202 {
+	String name;
+	int age;
+	String rollno;
+	 Rbca19202(String name, String rollno, int age)
+	 {
+		 this.name=name;
+		 this.rollno=rollno;
+		 this.age=age;
+	 }
+	 
+	 
+	 String getrollno()
+	 {
+		 return rollno;
+	 }
+
+
+}
